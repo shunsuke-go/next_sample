f97b8e0f28839c21872805b3a78cfc774aac0541
gon tana <gontana1220@gmail.com>
2022-06-20 10:40:36 +0900

    モーダルのカスタムフック作成

---
+++ src/hooks/use-modal.tsx
@@ -0,0 +1,42 @@
+import React, { useMemo } from 'react'
+import {
+  Modal as ModalCompornent,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@chakra-ui/react"
+
+type ModalProps = {
+  modalHeader?: React.ReactNode
+  modalBody?: React.ReactNode
+  modalFooter?: React.ReactNode
+}
+export const useModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const Modal: React.FC<ModalProps> = React.memo(({ modalHeader, modalBody, modalFooter }) => {
+    return (
+      <ModalCompornent isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            {modalHeader}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {modalBody}
+          </ModalBody>
+          <ModalFooter>
+            {modalFooter}
+          </ModalFooter>
+        </ModalContent>
+      </ModalCompornent>
+    )
+  })
+
+  return useMemo(() => ({ isOpen, onOpen, onClose, Modal }), [isOpen])
+}
