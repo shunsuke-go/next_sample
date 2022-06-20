4deabd8bbbc116fe4b8063140f4beaf67cf83248
gon tana <gontana1220@gmail.com>
2022-06-20 10:41:47 +0900

    認証関数開発

---
+++ src/utils/auth.ts
@@ -0,0 +1,10 @@
+import { parseCookies } from 'nookies'
+import { AxiosRequestConfig } from 'axios'
+
+export const auth = (config: AxiosRequestConfig = {}) => {
+  const cookies = parseCookies()
+  if (!cookies.token) return config
+  config.headers = {}
+  config.headers['Authorization'] = `Bearer ${cookies.token}`
+  return config
+}
