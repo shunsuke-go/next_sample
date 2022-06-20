c217ff712c5bcb1528ade4c1307d34e7ee26645a
gon tana <gontana1220@gmail.com>
2022-06-20 10:43:05 +0900

    userContextのカスタムフック開発

---
+++ src/hooks/use-user.tsx
@@ -0,0 +1,19 @@
+import React, { useContext, useState, createContext, PropsWithChildren, useMemo } from 'react'
+import { User } from '~/types/User'
+
+type UserContextType = [
+  User | undefined,
+  React.Dispatch<React.SetStateAction<User | undefined>>
+]
+const UserContext = createContext<UserContextType>([undefined, () => {}])
+
+export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [user, setUser] = useState<User>()
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = () => useContext<UserContextType>(UserContext)
